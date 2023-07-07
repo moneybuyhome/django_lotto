1caeffa33a4f4e9549cd395cb3a43bd791e88968
moneybuyhome <gw.lineage01@gmail.com>
2023-07-07 12:01:20 +0900

    first commit

---
+++ lotto/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import GuessNumbers
+# from lotto.models import GuessNumbers
+# Register your models here.
+admin.site.register(GuessNumbers)
+
+# Register your models here.
