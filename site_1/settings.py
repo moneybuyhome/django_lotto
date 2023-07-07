4df723987adc934374ed5b8c784e410075f6d70b
moneybuyhome <gw.lineage01@gmail.com>
2023-07-07 14:01:10 +0900

    key delete

--- site_1/settings.py
+++ site_1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e216tiwe(y+@0k6$)j3l)=72)qiwhf%#av2x-4zzfyzx0v=&y9'
+SECRET_KEY = 'django-insecure'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
